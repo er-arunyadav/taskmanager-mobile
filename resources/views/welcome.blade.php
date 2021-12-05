2ed7ba06fa56d2f5b33a076f3936fffac56deb3b
Arun  Yadav <arun.vnnovate@gmail.com>
2021-12-05 13:45:55 +0530

    Task manager init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Death Note</title>
+   
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body>
+    <nav>  
+        <ul class="nav">  
+          <li><i data-feather="menu"></i></li>  
+          <li class="title">Task Manager</li>  
+          <li><i data-feather="bell"></i></li>  
+        </ul>  
+    </nav>
+    <div class="container-fluid">
+        <div class="section">
+            <div class="section1-child">
+                <p>Welcome Back!</p>
+                <h3 class="updateToday">Here's Update Today.</h3>
+            </div>
+            <div class="section1-child">
+                <p class="searchicon"><i class="searchiconSize" data-feather="search"></i></p>
+            </div>
+        </div>
+        <div class="section mt-2">
+            <button class="section2btn active">Today</button>
+            <button class="section2btn">Upcoming</button>
+            <button class="section2btn">Task Done</button>
+        </div>
+        <div class="sectionCard mt-2">
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <div class="chipset">
+                        <button class="chip">Work</button>
+                        <button class="chip">Activity</button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h3>Read React Js</h3>
+                    <div class="remind">
+                        <div class="dateTimeReminder">
+                            <div class="calender">
+                                <i class="size-18" data-feather="calendar"></i>
+                                <p class="datereminder">12 December 2021</p>
+                            </div>
+                            <div class="calender">
+                                <i class="size-18" data-feather="clock"></i>
+                                <p class="datereminder">07:00(Remind At 07:35 )</p>
+                            </div>
+                        </div>
+
+                        <div class="circle">
+                            <i data-feather="circle"></i>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="floatingBtnDown">
+            <a href="#" class="float">
+                <div class="groupBtn">
+                    <p>&nbsp;Add Task</p>
+                    <i data-feather="plus-square" class="my-float"></i> 
+                </div>
+            </a>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script>
+        feather.replace()
+        $(document).ready(function(){
+            $(window).scroll(function() {
+                if((window.innerHeight + window.pageYOffset) >= document.body.scrollHeight - 2){
+                  
+                    $( ".float" ).addClass("floatDown" ,1000);
+                   
+                }else{
+                    
+                    $( ".float" ).removeClass("floatDown" ,1000);
+                   
+                }
+            });
+        })
+    </script>
+</body>
+</html>
